495648ca75451d126ff6981fc978605373c0a209
Laggo <5466315@qq.com>
2016-02-26 12:36:03 +0800

    readme

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,35 @@ var notifier = require('node-notifier');
 var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 var plumber = require('gulp-plumber');
+var cache = require('gulp-cached');
 var notify = require('gulp-notify');
+
+gulp.task('html', function () {
+    return gulp.src('./src/**/*.html')
+        .pipe(cache('linting'))
+        .pipe(gulp.dest('./dist/'));
+});
 
 gulp.task('script', function () {
     return gulp.src('src/js/**/*.js')
+        .pipe(cache('linting'))
         .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/js/'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('js/**/*.js', ['script']);
-    gulp.watch(['**/*.js','**/*.html'],browserSync.reload);
+    gulp.watch('src/**/*.js', ['script']);
+    gulp.watch('src/**/*.html', ['html']);
+    gulp.watch(['src/**/*.js','src/**/*.html'], browserSync.reload);
 });
 
+
 //default task
-gulp.task('default', ['watch','script'], function () {
+gulp.task('default', ['watch', 'script','html'], function () {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./src/"
         }
     });
     notifier.notify({
